c4bb3e16201c5ee88d8902c7a5ca17e5ac05c000
EgorPodvalkov <egorpodvalkov@gmail.com>
2024-03-06 12:59:28 +0200

    Optimized some sql in employee repository

--- Repositories/Employee/EmployeeRepository.cs
+++ Repositories/Employee/EmployeeRepository.cs
@@ -5,12 +5,22 @@ namespace TestUkrposhta.Repositories
 {
     public class EmployeeRepository : BaseRepository<Employee>, IEmployeeRepository
     {
-        private const string baseSelect =
-                @$"SELECT [Employees].*, [Companies].[Name] as CompanyName, [Positions].[Name] as PositionName, [Departaments].[Name] as DepartamentName 
-                    FROM [Employees]
-	                INNER JOIN [Positions] ON [Employees].[PositionID] = [Positions].[ID]	
-	                INNER JOIN [Companies] ON [Employees].[CompanyID] = [Companies].[ID]
-	                INNER JOIN [Departaments] ON [Employees].[DepartamentID] = [Departaments].[ID] ";
+        #region SQL const strings
+        /// <summary> Select employee with fields for displaying </summary>
+        private const string readSelect =
+        @$"SELECT 
+        [Employees].[ID], [Employees].[FullName], [Employees].[Address], [Employees].[PhoneNumber], [Employees].[Salary], [Employees].[BirthDate], [Employees].[HireDate],  
+        [Companies].[Name] as CompanyName, [Positions].[Name] as PositionName, [Departaments].[Name] as DepartamentName 
+            FROM [Employees]
+	        INNER JOIN [Positions] ON [Employees].[PositionID] = [Positions].[ID]	
+	        INNER JOIN [Companies] ON [Employees].[CompanyID] = [Companies].[ID]
+	        INNER JOIN [Departaments] ON [Employees].[DepartamentID] = [Departaments].[ID] ";
+
+        /// <summary> Select employee with fields for update </summary>
+        private const string updateSelect =
+        $@"SELECT [ID], [FullName], [Address], [PhoneNumber], [Salary], [BirthDate], [HireDate], [DepartamentID], [PositionID]
+            FROM [Employees] ";
+        #endregion
 
         public EmployeeRepository(string connectionString) : base("Employees", connectionString) { }
 
@@ -26,7 +36,7 @@ namespace TestUkrposhta.Repositories
 
         public override async Task<IEnumerable<Employee>> GetAllAsync()
         {
-            var sql = baseSelect;
+            var sql = readSelect;
 
             using var db = _dbConnection;
             return await db.QueryAsync<Employee>(sql);
@@ -38,7 +48,7 @@ namespace TestUkrposhta.Repositories
 
             var whereSegment = !string.IsNullOrEmpty(filterCondition) ? $"WHERE {filterCondition}" : string.Empty;
 
-            var sql = baseSelect + whereSegment;
+            var sql = readSelect + whereSegment;
 
             using var db = _dbConnection;
             return await db.QueryAsync<Employee>(sql);
@@ -72,7 +82,7 @@ namespace TestUkrposhta.Repositories
 
         public async override Task<Employee?> GetItemAsync(int id)
         {
-            var sql = baseSelect + "WHERE [Employees].[ID] = @id";
+            var sql = updateSelect + "WHERE [Employees].[ID] = @id";
 
             using var db = _dbConnection;
             return await db.QuerySingleOrDefaultAsync<Employee>(sql, new { id });
